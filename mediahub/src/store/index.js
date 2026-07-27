c086b72b735d0cd9168c2854dc0f3e5a5cff7db1
Kater <Katerstechhub@gmail.com>
2026-07-27 12:50:45 +0100

    innit

--- mediahub/src/store/index.js
+++ mediahub/src/store/index.js
@@ -1,5 +1,5 @@
 export { useAuthStore, default as useAuthStoreDefault } from './AuthStore'
-export { useThemeStore } from './themeStore'
+export { useThemeStore, initThemeListener } from './themeStore'
 export { usePostStore } from './postStore'
 export { useNotificationStore } from './notificationStore'
 export { useUIStore } from './uiStore'
